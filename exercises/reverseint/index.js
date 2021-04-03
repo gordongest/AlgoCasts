0fc513044f68e494c74494fefa05191339a1bda9
gordongest <gordon.gest@gmail.com>
2021-04-03 15:00:58 -0600

    implemented reverseInt without strings/arrays

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,20 +8,45 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {
-  let neg = false;
+/* solution 1 */
 
-  if (n < 0) {
-    neg = true;
+// function reverseInt(n) {
+//   let neg = false;
+
+//   if (n < 0) {
+//     neg = true;
+//   }
+
+//   const num = parseInt(Math.abs(n).toString().split('').reverse().join(''));
+
+//   if (neg) {
+//     return -num;
+//   }
+
+//   return num;
+// }
+
+/* solution 2 */
+
+// const reverseInt = (n) => {
+//   const reversed = n.toString().split('').reverse().join('');
+
+//   return parseInt(reversed) * Math.sign(n);
+// };
+
+/* solution 3 - no strings/arrays - PREFERABLE */
+
+const reverseInt = (n) => {
+  let reversedNum = 0;
+
+  let num = Math.abs(n);
+
+  while (num >= 1) {
+    reversedNum = reversedNum * 10 + num % 10;
+    num = Math.floor(num / 10);
   }
 
-  const num = parseInt(Math.abs(n).toString().split('').reverse().join(''));
-
-  if (neg) {
-    return -num;
-  }
-
-  return num;
+  return reversedNum * Math.sign(n);
 }
 
 module.exports = reverseInt;
--- exercises/reverseint/index.ts
+++ exercises/reverseint/index.ts
@@ -1,3 +1,5 @@
+/* solution 1 */
+
 const reverseInt = (n: number): number => {
   let neg = false;
 
@@ -5,17 +7,34 @@ const reverseInt = (n: number): number => {
     neg = true;
   }
 
-  const num = parseInt(
-    Math.abs(n)
-    .toString()
-    .split('')
-    .reverse()
-    .join('')
-  );
+  const num = parseInt(Math.abs(n).toString().split('').reverse().join(''));
 
   if (neg) {
     return -num;
   }
 
   return num;
+};
+
+/* solution 2 */
+
+const reverseInt2 = (n: number): number => {
+  const reversed: string = n.toString().split('').reverse().join('');
+
+  return parseInt(reversed) * Math.sign(n);
+};
+
+/* solution 3 - no strings/arrays - PREFERABLE */
+
+const reverseInt3 = (n: number): number => {
+  let reversedNum = 0;
+
+  let num = Math.abs(n);
+
+  while (num >= 1) {
+    reversedNum = reversedNum * 10 + num % 10;
+    num = Math.floor(num / 10);
+  }
+
+  return reversedNum * Math.sign(n);
 }
