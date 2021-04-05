2591f7c07e97ae05820748e3b71f026d68b2c91b
gordongest <gordon.gest@gmail.com>
2021-04-05 17:26:52 -0600

    added notes to steps, added recursive version

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -23,6 +23,33 @@ const steps = (n) => {
     // at each step, log i #'s and (n - i) spaces
     console.log('#'.repeat(i) + ' '.repeat(n - i));
   }
-}
+};
+
+// unnecessarily hard version
+
+const recursiveSteps = (n, row = 0, str = '') => {
+  // if we have reached n rows, we're done
+  if (row === n) {
+    return;
+  }
+
+  if (n === str.length) {
+    // if the string has n characters in it, print it and
+    console.log(str);
+    // proceed to the next row
+    return recursiveSteps(n, row + 1);
+  }
+
+  if (str.length <= row) {
+    // if the string length is less than/equal to the row, add a #
+    str += '#';
+  } else {
+    // otherwise add whitespace until the row is complete
+    str += ' ';
+  }
+
+  // recursive call to continue building the current row
+  recursiveSteps(n, row, str);
+};
 
 module.exports = steps;
