47c93648f54196ac955611348e1e7e1c3aec3588
gordongest <gordon.gest@gmail.com>
2021-04-08 16:55:17 -0600

    completed QfromS

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Implement a Queue datastructure using two stacks.
+// Implement a Queue data structure using two stacks.
 // *Do not* create an array inside of the 'Queue' class.
 // Queue should implement the methods 'add', 'remove', and 'peek'.
 // For a reminder on what each method does, look back
@@ -14,6 +14,43 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.a = new Stack();
+    this.b = new Stack();
+  }
+
+  add(record) {
+    this.a.push(record);
+  }
+
+  remove() {
+    while (this.a.peek()) {
+      this.b.push(this.a.pop());
+    }
+
+    const record = this.b.pop();
+
+    while (this.b.peek()) {
+      this.a.push(this.b.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.a.peek()) {
+      this.b.push(this.a.pop());
+    }
+
+    const record = this.b.peek();
+
+    while(this.b.peek()) {
+      this.a.push(this.b.pop());
+    }
+
+    return record;
+  }
+}
 
 module.exports = Queue;
