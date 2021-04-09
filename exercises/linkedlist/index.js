1ec0a683b813d7c0bbd1fc2808e723a80e8dcaf7
gordongest <gordon.gest@gmail.com>
2021-04-08 21:44:33 -0600

    completed LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -14,9 +14,17 @@ class LinkedList {
     this.head = null;
   }
 
+  checkHead() {
+    if (!this.head) {
+      return;
+    }
+  }
+
   insertFirst(data) {
-    const node = new Node(data, this.head);
-    this.head = node;
+    // const node = new Node(data, this.head);
+    // this.head = node;
+
+    this.insertAt(data, 0);
   }
 
   size() {
@@ -32,21 +40,25 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head;
+    // return this.head;
+
+    return this.getAt(0);
   }
 
   getLast() {
-    if (!this.head) {
-      return null;
-    }
+    // if (!this.head) {
+    //   return null;
+    // }
 
-    let node = this.head;
+    // let node = this.head;
 
-    while (node.next) {
-      node = node.next;
-    }
+    // while (node.next) {
+    //   node = node.next;
+    // }
 
-    return node;
+    // return node;
+
+    return this.getAt(this.size() - 1)
   }
 
   clear() {
@@ -54,60 +66,131 @@ class LinkedList {
   }
 
   removeFirst() {
-    if (!this.head) {
-      return;
-    }
+    // if (!this.head) {
+    //   return;
+    // }
 
-    this.head = this.head.next;
+    // this.head = this.head.next;
+
+    this.removeAt(0);
   }
 
   removeLast() {
-    if (!this.head) {
-      return;
-    }
+    // if (!this.head) {
+    //   return;
+    // }
 
-    if (!this.head.next) {
-      this.head = null;
-      return;
-    }
+    // if (!this.head.next) {
+    //   this.head = null;
+    //   return;
+    // }
 
-    let prev = this.head;
-    let node = this.head.next;
+    // let prev = this.head;
+    // let node = this.head.next;
 
-    while (node.next) {
-      prev = node;
-      node = node.next;
-    }
+    // while (node.next) {
+    //   prev = node;
+    //   node = node.next;
+    // }
 
-    prev.next = null;
+    // prev.next = null;
+
+    this.removeAt(this.size() - 1);
   }
 
   insertLast(data) {
-    if (!this.head) {
-      this.head = new Node(data);
-    }
+    // if (!this.head) {
+    //   this.head = new Node(data);
+    //   return;
+    // }
 
+    // const node = this.getLast();
+
+    // node.next = new Node(data);
+
+    this.insertAt(data, this.size());
+  }
+
+  getAt(index) {
+    let counter = 0;
     let node = this.head;
 
-    while (node.next) {
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
       node = node.next;
     }
 
-    node.next = new Node(data);
+    return null;
   }
 
-  getAt(n) {}
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
 
-  removeAt(n) {}
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
 
-  insertAt(data, n) {}
+    const prev = this.getAt(index - 1);
+    /* this works, but less efficient because it requires an add'l loop */
+    // const next = this.getAt(index + 1);
+
+    if (!prev || !prev.next) {
+      return;
+    }
+
+    prev.next = prev.next.next;
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (index === 0) {
+      const next = this.head;
+      this.head = new Node(data, next);
+      return;
+    }
+
+    const prev = this.getAt(index - 1) || this.getLast();
+
+    /* inefficient? */
+    // if (!prev) {
+    //   this.insertLast(data);
+    //   return;
+    // }
+
+    const next = prev.next;
+    prev.next = new Node(data, next);
+  }
 
   forEach(fn) {
-    let current = this.head;
+    let node = this.head;
+    // let counter = 0;
 
-    while (current.next) {
-      fn(current.data);
-      current = current.next;
+    while (node) {
+      fn(node);
+      node = node.next;
+      // counter++;
+    }
+  }
+
+  // generator function
+  *[Symbol.iterator]() {
+    let node = this.head;
+
+    while (node) {
+      /* at each stop it returns the node */
+      yield node;
+      node = node.next;
     }
   }
 }
